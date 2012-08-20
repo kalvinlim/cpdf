0c327b5ebe6416f1308b02e08ee62cd55a778b0d
Min the Master <kminchan@gmail.com>
2012-08-20 13:03:33 -0400

    push

--- ClusterFuck/src/com/example/clusterfuck/MainActivity.java
+++ ClusterFuck/src/com/example/clusterfuck/MainActivity.java
@@ -16,12 +16,46 @@ import android.view.MenuItem;
 import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
+
+//Following Imports are for the Camera API
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import com.example.clusterfuck.R;
+
+
 public class MainActivity extends Activity {
+	//--coding for Camera API
+	private final static String DEBUG_TAG = "MainActivity";
+	private Camera camera;
+	private int cameraId = 0;
+	//--END for coding for Camera API
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //--START for coding for Camera API
+        if (!getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+              Toast.makeText(this, "No camera on this device", Toast.LENGTH_LONG)
+                  .show();
+            } else {
+              cameraId = findFrontFacingCamera();
+              camera = Camera.open(cameraId);
+              if (cameraId < 0) {
+                Toast.makeText(this, "No front facing camera found.",
+                    Toast.LENGTH_LONG).show();
+              }
+            }
+        //--END for coding for Camera API
+        
         Toast msg = Toast.makeText(MainActivity.this,
                 "File Path: " + android.os.Environment.getExternalStorageDirectory(), Toast.LENGTH_LONG);
         msg.show();
@@ -57,6 +91,37 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-
+    
+    //--START for coding for Camera API
+    public void onClick(View view) {
+        camera.takePicture(null, null,
+            new PhotoHandler(getApplicationContext()));
+    }
+    
+    private int findFrontFacingCamera() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+          CameraInfo info = new CameraInfo();
+          Camera.getCameraInfo(i, info);
+          if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
+            Log.d(DEBUG_TAG, "Camera found");
+            cameraId = i;
+            break;
+          }
+        }
+        return cameraId;
+     }
+    
+    @Override
+    protected void onPause() {
+      if (camera != null) {
+        camera.release();
+        camera = null;
+      }
+      super.onPause();
+    }
+    //END for coding for Camera API
     
 }
